4ad3712c01d656e69b82312b662673af9f6477b9
njange <jamesnjange80@gmail.com>
2024-07-25 11:18:15 +0300

    get request previously sent to test directory only

--- api/index.js
+++ api/index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req,res)=>{
+    res.json('Hello');
+});
+
 app.get('/test', (req,res)=>{
     req.json('test ok');
 });
-app.listen(4000)
+app.listen(4000, () => {
+    console.log('Server is running on port 4000');
+});
