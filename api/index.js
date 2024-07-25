31d90f7cb206b6d23184cd0188c7163d48fb2488
Ken Mutuku <you@example.com>
2024-07-25 12:37:58 +0300

    back on track

--- api/index.js
+++ api/index.js
@@ -6,8 +6,10 @@ const app = express();
 // });
 
 app.get('/test', (req,res)=>{
-    req.json('test ok');
+    res.json('test ok');
 });
-app.listen(4000, () => {
-    console.log('Server is running on port 4000');
-});
+
+app.listen(4000);
+// , () => {
+//     console.log('Server is running on port 4000');
+// });
